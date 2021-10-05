a38ffd2a17fb3a8f09cd38ab075e25a81990ab75
andrew hazlett <andyhaz1970@gamile.com>
2021-10-05 14:18:08 -0500

    Initial Commit

---
+++ swift5-SpritKit-templateUITests/swift5_SpritKit_templateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift5_SpritKit_templateUITestsLaunchTests.swift
+//  swift5-SpritKit-templateUITests
+//
+//  Created by andyhaz on 10/5/21.
+//
+
+import XCTest
+
+class swift5_SpritKit_templateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
